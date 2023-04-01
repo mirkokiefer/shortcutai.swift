af42978a7bb899d702c9fa0aab6c8670c92a12db
Mirko Kiefer <mail@mirkokiefer.com>
2023-04-01 20:49:50 +0700

    🔥 refactor(streams.swift): replace JSONStreamer class with SSEStream struct and URLSessionIterator class ✨ feat(streams.swift): add support for async iteration over Server-Sent Events using URLSessionIterator class

--- playground/streams/streams.swift
+++ playground/streams/streams.swift
@@ -1,103 +1,86 @@
-// main.swift
 import Foundation
 
-class JSONStreamer: NSObject {
-    let url: URL
-    let outputFile: URL
-    var session: URLSession!
-    var task: URLSessionDataTask?
-    let sseEventParser = ServerSentEventParser()
+struct SSEStream: AsyncSequence {
+    let urlRequest: URLRequest
 
-    init(url: URL, outputFile: URL) {
-        self.url = url
-        self.outputFile = outputFile
-        super.init()
-        self.session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-    }
+    typealias Element = Result<String, Error>
+    typealias AsyncIterator = URLSessionIterator
 
-    func startStreaming() {
-        print("Start streaming")
-
-        var request = URLRequest(url: url)
-        request.setValue("text/event-stream", forHTTPHeaderField: "Accept")
-
-        task = session.dataTask(with: request)
-        task?.resume()
-    }
-
-    func appendJSONToFile(jsonString: String) throws {
-        let data = (jsonString + "\n").data(using: .utf8)!
-        try append(data: data, to: outputFile)
-    }
-    
-    func append(data: Data, to url: URL) throws {
-        if let fileHandle = try? FileHandle(forWritingTo: url) {
-            defer { fileHandle.closeFile() }
-            fileHandle.seekToEndOfFile()
-            fileHandle.write(data)
-        } else {
-            try data.write(to: url)
-        }
+    func makeAsyncIterator() -> URLSessionIterator {
+        return URLSessionIterator(urlRequest: urlRequest)
     }
 }
 
-extension JSONStreamer: URLSessionDataDelegate {
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        print("Parsing data")
-        sseEventParser.parse(data: data) { json in
-            print(json)
-            do {
-                try self.appendJSONToFile(jsonString: json)
-                print("JSON appended to file. \(json)")
-            } catch {
-                print("Error appending JSON to file: \(error)")
-            }
+final class URLSessionIterator: NSObject, AsyncIteratorProtocol, URLSessionDataDelegate, URLSessionTaskDelegate, URLSessionDelegate {
+    let urlRequest: URLRequest
+    var buffer: String = ""
+    let task: URLSessionDataTask
+    var continuation: CheckedContinuation<Element, Never>?
+
+    init(urlRequest: URLRequest) {
+        self.urlRequest = urlRequest
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        self.task = session.dataTask(with: urlRequest)
+    }
+
+    func next() async -> Element? {
+        await withOptionalCheckedContinuation { continuation in
+            self.continuation = continuation
+            self.task.resume()
         }
     }
 
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         if let error = error {
-            print("Error: \(error)")
-            return
+            continuation?.resume(throwing: error)
+        } else {
+            continuation?.resume(returning: nil)
         }
-        print("Task completed.")
+    }
+
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        guard let chunk = String(data: data, encoding: .utf8) else { return }
+        buffer += chunk
+        while let (event, remaining) = extractEvent(buffer) {
+            buffer = remaining
+            continuation?.resume(returning: .success(event))
+        }
     }
 }
 
-// ServerSentEventParser.swift
-import Foundation
-
-class ServerSentEventParser {
-    private var buffer = ""
-
-    func parse(data: Data, eventHandler: (String) -> Void) {
-        if let chunk = String(data: data, encoding: .utf8) {
-            buffer += chunk
-
-            while let (event, remaining) = extractEvent(buffer) {
-                buffer = remaining
-                eventHandler(event)
-            }
-        }
-    }
-
-    private func extractEvent(_ string: String) -> (String, String)? {
-        if let range = string.range(of: "\n\n") {
+private func extractEvent(_ string: String) -> (String, String)? {
+    if let range = string.range(of: "\n\n") {
             let event = String(string[string.startIndex..<range.lowerBound])
             let remaining = String(string[range.upperBound...])
             return (event, remaining)
         }
         return nil
+}
+
+func sseEvents(for request: URLRequest) -> SSEStream {
+    return SSEStream(urlRequest: request)
+}
+
+// Example usage:
+
+let url = URL(string: "https://example.com/sse")!
+let request = URLRequest(url: url)
+
+let sseStream = sseEvents(for: request)
+
+Task {
+    do {
+        for try await result in sseStream {
+            switch result {
+            case .success(let event):
+                print("Received event: \(event)")
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
+    } catch {
+        print("Caught error: \(error)")
     }
 }
 
-let streamURL = URL(string: "http://localhost:8000/api/stream_test")!
-// outputFile relative to the cwd path
-let outputFile = URL(fileURLWithPath: "output.json")
-
-
-let jsonStreamer = JSONStreamer(url: streamURL, outputFile: outputFile)
-
-jsonStreamer.startStreaming()
-
-RunLoop.main.run(until: Date(timeIntervalSinceNow: 15))
+RunLoop.main.run()
